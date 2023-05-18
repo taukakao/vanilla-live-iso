baef3977d3504868d8ca806ab662bdb5d5ea65b5
Mirko Brombin <send@mirko.pm>
2023-05-18 08:26:26 +0200

    Live image only (#190)
    Changes
    - use the pico image as environment
    - remove squashfs generation
    - setup a minimal gnome environment removing extra packages
    - removed vanilla os repository configuration in build.sh
    
    Know problems
    - no vanilla wallpaper (vanilla-gnome-default-settings need to be migrated to orchid)

--- build.sh
+++ build.sh
@@ -19,33 +19,17 @@ source "$BASE_DIR"/"$CONFIG_FILE"
 
 echo -e "
 #----------------------#
-# INSTALL VANILLA REPO #
-#----------------------#
-"
-
-# Add vanilla sources
-cat > /etc/apt/sources.list.d/vanilla-base.list <<EOF
-deb [arch=amd64] http://repo.vanillaos.org/ $BASECODENAME main
-EOF
-
-# Add vanilla repo key
-apt-key add "$BASE_DIR"/etc/config/archives/vanilla.key
-apt-key add "$BASE_DIR"/etc/config/archives/vanilla-main.key
-
-# Add vanilla keyring
-cp "$BASE_DIR"/etc/config/includes.chroot/usr/share/keyrings/vanilla_keyring.gpg /usr/share/keyrings/
-
-# Remove stock debian sources
-rm -f /etc/apt/sources.list.d/debian.sources
-
-echo -e "
-#----------------------#
 # INSTALL DEPENDENCIES #
 #----------------------#
 "
 apt-get update
 apt-get install -y live-build gnupg2 binutils zstd ca-certificates
 
+echo -e "
+#----------------------#
+# PREPARE BUILD OUTPUT #
+#----------------------#
+"
 build () {
   BUILD_ARCH="$1"
   mkdir -p "$BASE_DIR/tmp/$BUILD_ARCH"
@@ -90,7 +74,7 @@ build () {
   OUTPUT_DIR="$BASE_DIR/builds/$BUILD_ARCH"
   mkdir -p "$OUTPUT_DIR"
   FNAME="VanillaOS-$VERSION-$CHANNEL.$YYYYMMDD$OUTPUT_SUFFIX"
-  mv "$BASE_DIR/tmp/$BUILD_ARCH/live-image-$BUILD_ARCH.hybrid.iso" "$OUTPUT_DIR/${FNAME}.iso"
+  mv $BASE_DIR/tmp/amd64/live-image-amd64.iso "$OUTPUT_DIR/${FNAME}.iso"
 
   # cd into output to so {FNAME}.sha256.txt only
   # includes the filename and not the path to
